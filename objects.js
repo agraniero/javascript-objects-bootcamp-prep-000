7eb15730e569dfe49060a5650a19120a4fdecfa5
Anthony Graniero <ananfro@gmail.com>
2018-10-04 11:55:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,5 +5,6 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, key, value) {
-  return Object.assign({}, obj, {playlist [TheCure]} = 'Object'
+  playlist[TheCure] = 'Object';
+  return playlist;
 }
